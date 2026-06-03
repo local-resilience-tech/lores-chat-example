72c899cbacdb0b7135054cca8c068d35a046d589
jade <jade@hopepunk.me>
2026-06-03 17:47:43 +1000

    don't put name in identity

--- frontend/src/identity.js
+++ frontend/src/identity.js
@@ -4,5 +4,5 @@ const ANIMALS = ["bear", "fox", "wolf", "owl", "deer", "lynx", "hawk", "frog"];
 export function generateIdentity() {
   const colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
   const animal = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
-  return { colour, animal, name: `${colour} ${animal}` };
+  return { colour, animal };
 }
